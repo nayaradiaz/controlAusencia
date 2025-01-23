665247c78a2b629f6ef3f3fa16673b1cf6728dba
nayaradiaz <164243517+nayaradiaz@users.noreply.github.com>
2025-01-23 17:40:57 +0000

    commit

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,10 +14,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('surnames');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-           $table->foreignId('department_id')->constrained()->onDelete('cascade');
-
+            $table->foreignId('department_id');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
