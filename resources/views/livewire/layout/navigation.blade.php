ad754f22197992796f4a25cdfb111aa159fdeaa7
nayaradiaz <164243517+nayaradiaz@users.noreply.github.com>
2025-01-24 15:52:33 +0000

    commit

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -31,7 +31,19 @@ new class extends Component
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
-                        {{ __('Dashboard') }}
+                        {{ __('Registrar Usuarios') }}
+                    </x-nav-link>
+                </div>
+                 <!-- Navigation Links -->
+                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
+                        {{ __('Ausencias') }}
+                    </x-nav-link>
+                </div>
+                 <!-- Navigation Links -->
+                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
+                        {{ __('Faltas Comunicadas') }}
                     </x-nav-link>
                 </div>
             </div>
