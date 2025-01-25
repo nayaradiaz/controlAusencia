2f714a997b1ae003e2a62832a6e9bb88973feed2
Nayara <nayaradleon@gmail.com>
2025-01-25 01:03:59 +0000

    fallo_componente

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,6 +14,7 @@
 
                     @role('Admin')
                     @livewire('admin-users')
+
                     @endrole
                 </div>
             </div>
--- resources/views/livewire/admin-users.blade.php
+++ resources/views/livewire/admin-users.blade.php
@@ -1,30 +1,53 @@
 <div class="container">
     <h2>Registro Individual de Profesores</h2>
+    <div>
+        <form wire:submit.prevent="registerUser">
+            <!-- Name -->
+            <div>
+                <x-input-label for="name" :value="__('Name')" />
+                <x-text-input wire:model="name" id="name" class="block mt-1 w-full" type="text" name="name" required autofocus autocomplete="name" />
+                <x-input-error :messages="$errors->get('name')" class="mt-2" />
+            </div>
 
-    <!-- Formulario de registro individual -->
-    <form wire:submit.prevent="registerUser">
-        <div>
-            <label for="name">Nombre:</label>
-            <input type="text" id="name" wire:model="name" required>
-        </div>
-        <div>
-            <label for="surnames">Apellidos:</label>
-            <input type="text" id="surnames" wire:model="surnames" required>
-        </div>
-        <div>
-            <label for="email">Correo Electrónico:</label>
-            <input type="email" id="email" wire:model="email" required>
-        </div>
-        <div>
-            <label for="password">Contraseña:</label>
-            <input type="password" id="password" wire:model="password" required>
-        </div>
-        <div>
-            <label for="password_confirmation">Confirmar Contraseña:</label>
-            <input type="password" id="password_confirmation" wire:model="password_confirmation" required>
-        </div>
-        <button type="submit">Registrar Profesor</button>
-    </form>
+            <!-- Email Address -->
+            <div class="mt-4">
+                <x-input-label for="email" :value="__('Email')" />
+                <x-text-input wire:model="email" id="email" class="block mt-1 w-full" type="email" name="email" required autocomplete="username" />
+                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            </div>
+
+            <!-- Password -->
+            <div class="mt-4">
+                <x-input-label for="password" :value="__('Password')" />
+                <x-text-input wire:model="password" id="password" class="block mt-1 w-full"
+                    type="password"
+                    name="password"
+                    required autocomplete="new-password" />
+                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            </div>
+
+            <!-- Confirm Password -->
+            <div class="mt-4">
+                <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+                <x-text-input wire:model="password_confirmation" id="password_confirmation" class="block mt-1 w-full"
+                    type="password"
+                    name="password_confirmation" required autocomplete="new-password" />
+                <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}" wire:navigate>
+                    {{ __('Already registered?') }}
+                </a>
+
+                <x-primary-button class="ms-4">
+                    {{ __('Register') }}
+                </x-primary-button>
+            </div>
+        </form>
+    </div>
+
+
 
     <!-- Formulario de carga de CSV -->
     <form wire:submit.prevent="uploadCsv" enctype="multipart/form-data">
@@ -37,30 +60,30 @@
 
     <!-- Mensajes de éxito -->
     @if(session('success'))
-        <div style="color: green;">
-            {{ session('success') }}
-        </div>
+    <div style="color: green;">
+        {{ session('success') }}
+    </div>
     @endif
 
     <!-- Mostrar errores de validación -->
     @error('csv_file')
-        <div style="color: red;">
-            {{ $message }}
-        </div>
+    <div style="color: red;">
+        {{ $message }}
+    </div>
     @enderror
     @error('name')
-        <div style="color: red;">
-            {{ $message }}
-        </div>
+    <div style="color: red;">
+        {{ $message }}
+    </div>
     @enderror
     @error('email')
-        <div style="color: red;">
-            {{ $message }}
-        </div>
+    <div style="color: red;">
+        {{ $message }}
+    </div>
     @enderror
     @error('password')
-        <div style="color: red;">
-            {{ $message }}
-        </div>
+    <div style="color: red;">
+        {{ $message }}
+    </div>
     @enderror
 </div>
